069e319634491fc6d11ef54648b5b48765ea6f7a
Ryannmouse <ryandsantosoliveira@gmail.com>
2024-11-30 22:51:13 -0300

    modified:   Trabalho.py

--- Trabalho.py
+++ Trabalho.py
@@ -7,14 +7,6 @@ class Vazia(Exception):
   ''' Erro ao tentar acessar um elemento de um contêiner vazio.'''
   pass
 
-
-
-
-
-
-
-# arthur viado
-
 class PilhaArray:
   ''' Implementação de pilha LIFO usando uma lista Python como armazenamento subjacente'''
 
@@ -26,11 +18,11 @@ class PilhaArray:
     ''' retorna o número de elementos na pilha'''
     return len(self._dados)
 
-  def esta_vazia(self):
+  def lista_vazia(self):
     ''' Retorna True se a pilha estiver vazia'''
     return len(self._dados) == 0
 
-  def empilhar(self, e):
+  def push(self, e):
     ''' Adiciona o elemento e ao topo da pilha'''
     self._dados.append(e)  # novo item armazenado no final da lista
   
@@ -43,7 +35,7 @@ class PilhaArray:
       raise Vazia('A pilha está vazia')
     return self._dados[-1]  # o último item na lista
 
-  def desempilhar(self):
+  def pop(self):
     ''' 
     Remove e retorna o elemento do topo da pilha 
     Levanta a exceção Vazia se a pilha estiver vazia
