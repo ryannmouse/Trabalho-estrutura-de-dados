434c49b9f77222fc37b905f3a653996589137fce
Rodrigo Sousa Barbosa <108591857+rodrigosousa13@users.noreply.github.com>
2024-12-06 22:04:20 -0300

    Método liberar atualizado, documentações implementadas/melhoradas

--- questao_2.py
+++ questao_2.py
@@ -4,61 +4,67 @@
 
 # define um novo tipo de exceção para o TAD pilha
 class Vazia(Exception):
-  ''' Erro ao tentar acessar um elemento de um contêiner vazio.'''
+  '''Erro ao tentar acessar um elemento de um contêiner vazio.'''
   pass
 
 class PilhaArray:
-  ''' Implementação de pilha LIFO usando uma lista Python como armazenamento subjacente'''
+  '''Implementação de pilha LIFO usando uma lista Python como armazenamento subjacente'''
 
   def __init__(self):
-    ''' cria uma pilha vazia'''
+    '''Cria uma pilha vazia'''
     self._dados = []    # instância de lista não pública
 
   def _len_(self):
-    ''' retorna o número de elementos na pilha'''
+    '''Retorna o número de elementos na pilha'''
     return len(self._dados)
 
   def esta_vazia(self):
-    ''' Retorna True se a pilha estiver vazia'''
+    '''Retorna True se a pilha estiver vazia'''
     return len(self._dados) == 0
 
   def push(self, e):
-    ''' Adiciona o elemento e ao topo da pilha'''
+    '''Adiciona o elemento e ao topo da pilha'''
     self._dados.append(e)  # novo item armazenado no final da lista
   
   def top(self):
-    ''' Retorna o elemento no topo da pilha'''
+    '''Retorna o elemento no topo da pilha'''
     if self.esta_vazia():
       raise Vazia('A pilha está vazia')
     return self._dados[-1]  # o último item na lista
 
   def pop(self):
-    ''' Remove e retorna o elemento do topo da pilha'''
+    '''Remove e retorna o elemento do topo da pilha'''
     if self.esta_vazia():
       raise Vazia('A pilha está vazia')
     return self._dados.pop()
 
   def _str_(self):
-    ''' Uma representação em string da pilha'''
+    '''Uma representação em string da pilha'''
     return ''.join(str(self._dados)) + '>'
   
   def imprimir(self):
+   '''Imprime a pilha'''
    print(self._str_())
  
   def imprimir_reversa(self):
+    '''Imprime a pilha de forma reversa'''
     reverso = '<' + ''.join(str(self._dados[::-1]))
     print(reverso)
 
   def liberar(self):
-    pass
+    '''Remove todos os elementos da pilha'''
+    for _ in range(len(self._dados)):
+      self._dados.pop()
 
   def palindromo(self):
+    '''Verifica se a pilha é um palíndromo'''
     if self._dados == self._dados[::-1]:
       return True
     else:
       return False
     
   def elimina(self, elemento):
+    '''Remove um elemento da pilha'''
     elementos_de_cima = []
     achou = False
     for _ in range(len(self._dados)):
@@ -75,6 +81,7 @@ class PilhaArray:
       self.push(elemento)
 
   def pares_e_impares(self):
+    '''Separa os números pares e ímpares em duas pilhas diferentes'''
     # pedir os números para o usuário
     pilha = PilhaArray()
 
