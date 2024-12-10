2c480a9a3a0d38e703ff043d967baa976c9e0bc4
Rodrigo Sousa Barbosa <108591857+rodrigosousa13@users.noreply.github.com>
2024-12-10 11:15:17 -0300

    Melhorias e implementações na questão 4

--- questao_4.py
+++ questao_4.py
@@ -9,10 +9,22 @@ def valida_expressao(expressao):
         if caractere in '({[':
             pilha.push(caractere)
         elif caractere in ')}]':
-            if pilha.is_empty():
+            if pilha.esta_vazia():
                 return False
             topo = pilha.pop()
             if (topo == '(' and caractere != ')') or \
                 (topo == '[' and caractere != ']') or \
                 (topo == '{' and caractere != '}'):
                 return False
+            
+    return pilha.esta_vazia() # Se a pilha terminar vazia, a expressão é válida
+
+def main():
+    expressao = input('Digite uma expressão matemática: ')
+    if valida_expressao(expressao):
+        print('Expressão válida!')
+    else:
+        print('Expressão inválida!')
+
+if __name__ == '__main__':
+    main()
