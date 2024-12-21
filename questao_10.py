92ddade43c355acc7e9ebe15d356b8c034e8d3e2
arthurt1 <162047312+arthurt1@users.noreply.github.com>
2024-12-20 22:36:21 -0300

    Update questao_10.py

--- questao_10.py
+++ questao_10.py
@@ -1,55 +1,55 @@
-class No:
+class Node:
     def __init__(self, valor):
-        self.valor = valor
-        self.anterior = None
-        self.proximo = None
+        self.valor = valor        
+        self.anterior = None     
+        self.proximo = None       
 
 class DequeDinamica:
     def __init__(self):
-        self.inicio = None
-        self.fim = None
-
+        self.inicio = None  
+        self.fim = None     
+        
     def deque_e_vazia(self):
         return self.inicio is None
 
     def insere_inicio_deque(self, valor):
-        novo_no = No(valor)
+        newest = Node(valor)
         if self.deque_e_vazia():
-            self.inicio = self.fim = novo_no 
+            self.inicio = self.fim = newest  
         else:
-            novo_no.proximo = self.inicio
-            self.inicio.anterior = novo_no
-            self.inicio = novo_no
+            newest.proximo = self.inicio    
+            self.inicio.anterior = newest   
+            self.inicio = newest            
 
     def insere_final_deque(self, valor):
-        novo_no = No(valor)
+        newest = Node(valor)
         if self.deque_e_vazia():
-            self.inicio = self.fim = novo_no 
+            self.inicio = self.fim = newest 
         else:
-            novo_no.anterior = self.fim
-            self.fim.proximo = novo_no
-            self.fim = novo_no
+            newest.anterior = self.fim      
+            self.fim.proximo = newest       
+            self.fim = newest              
 
     def remove_inicio_deque(self):
         if self.deque_e_vazia():
             raise IndexError("Deque está vazio!")
-        valor = self.inicio.valor
-        if self.inicio == self.fim:
+        valor = self.inicio.valor            
+        if self.inicio == self.fim:          
             self.inicio = self.fim = None
         else:
-            self.inicio = self.inicio.proximo
-            self.inicio.anterior = None
+            self.inicio = self.inicio.proximo  
+            self.inicio.anterior = None        
         return valor
 
     def remove_final_deque(self):
         if self.deque_e_vazia():
             raise IndexError("Deque está vazio!")
-        valor = self.fim.valor
-        if self.inicio == self.fim:
+        valor = self.fim.valor            
+        if self.inicio == self.fim:          
             self.inicio = self.fim = None
         else:
-            self.fim = self.fim.anterior
-            self.fim.proximo = None
+            self.fim = self.fim.anterior      
+            self.fim.proximo = None          
         return valor
 
     def imprimir(self):
