176910a71eda6d566f928faa7d9a109c0a460d36
arthurt1 <162047312+arthurt1@users.noreply.github.com>
2024-12-20 23:03:01 -0300

    Comentários da questão 10

--- questao_10.py
+++ questao_10.py
@@ -1,11 +1,14 @@
 class Node:
+    
     def __init__(self, valor):
+        '''Função para criar o nó'''
         self.valor = valor        
         self.anterior = None     
         self.proximo = None       
 
 class DequeDinamica:
     def __init__(self):
+        '''Inicializa Deque'''
         self.inicio = None  
         self.fim = None     
         
@@ -13,20 +16,21 @@ class DequeDinamica:
         return self.inicio is None
 
     def insere_inicio_deque(self, valor):
+        '''Função inserir no inicio'''
         newest = Node(valor)
         if self.deque_e_vazia():
-            self.inicio = self.fim = newest  
+            self.inicio = self.fim = newest  ## Caso seja o primeiro valor inserido, define fim e inicio como newest  
         else:
-            newest.proximo = self.inicio    
+            newest.proximo = self.inicio ## Caso contrario, insere no inicio e define o novo nó como inicio
             self.inicio.anterior = newest   
             self.inicio = newest            
 
     def insere_final_deque(self, valor):
         newest = Node(valor)
         if self.deque_e_vazia():
-            self.inicio = self.fim = newest 
+            self.inicio = self.fim = newest ## Caso seja o primeiro valor inserido, define fim e inicio como newest
         else:
-            newest.anterior = self.fim      
+            newest.anterior = self.fim ## Caso contrario, insere no final e define o novo nó como final
             self.fim.proximo = newest       
             self.fim = newest              
 
@@ -35,9 +39,9 @@ class DequeDinamica:
             raise IndexError("Deque está vazio!")
         valor = self.inicio.valor            
         if self.inicio == self.fim:          
-            self.inicio = self.fim = None
+            self.inicio = self.fim = None ## Caso haja apenas um único valor, a lista se torna vazia
         else:
-            self.inicio = self.inicio.proximo  
+            self.inicio = self.inicio.proximo  ## Caso contrario, remove do inicio e define o próximo como inicio
             self.inicio.anterior = None        
         return valor
 
@@ -46,18 +50,18 @@ class DequeDinamica:
             raise IndexError("Deque está vazio!")
         valor = self.fim.valor            
         if self.inicio == self.fim:          
-            self.inicio = self.fim = None
+            self.inicio = self.fim = None ## Caso haja apenas um único valor, a lista se torna vazia
         else:
-            self.fim = self.fim.anterior      
+            self.fim = self.fim.anterior ## Caso contrario, remove do final e define o anterior como final
             self.fim.proximo = None          
         return valor
 
     def imprimir(self):
         if self.deque_e_vazia():
-            print("Deque está vazio!")
+            print("Deque está vazio!") ## Caso a deque esteja vazia, imprime uma mesagem
             return
         atual = self.inicio
         while atual:
-            print(atual.valor, end=" ")
+            print(atual.valor, end=" ") ## Caso contrario, imprime todos os valores da lista
             atual = atual.proximo
         print()
